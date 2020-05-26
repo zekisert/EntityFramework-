aafdfac94c9ddd2148ea33b0ca9f5ecc3e281b22
zekisert <49846775+zekisert@users.noreply.github.com>
2020-05-26 03:24:40 +0300

    ETradeContext.cs

---
+++ ETradeContext.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ef
+{
+    public class ETradeContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
